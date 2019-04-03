15095a630f5e122ccd8aa01432266cc6b198b21a
Byron Mejia <hello@byronis.me>
2019-04-03 18:09:50 +1000

    fix(seo): add keywords and description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,22 @@ import { Profile } from '../components/home/profile';
 const IndexPage = () => (
     <GlobalStyleWrapper>
         <SEO
-            title='Byron Mejia'
-            keywords={[`programming`, `health`, `developer`, `fitness`, `life`, `blog`]}
+            title="Full Stack Developer, Tooling Fanatic, Dog Owner"
+            keywords={[
+                'programming',
+                'health',
+                'developer',
+                'fitness',
+                'life',
+                'blog',
+                'Brisbane',
+                'digital',
+                'Angular',
+                'React',
+                'TypeScript',
+            ]}
+            // tslint:disable-next-line: max-line-length
+            description="I am a full stack developer based in Brisbane, Australia. Follow me on my life journey in programming, health and being a dog owner. Contact me if you need help with Angular, React, Typescript and node.js"
         />
         <ThemeProvider theme={PrimaryTheme}>
             <HomeLayout>
