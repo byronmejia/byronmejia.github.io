c77a5a231a6afae2a3aa1e834c99fac37d44bfe7
Byron Mejia <hello@byronis.me>
2019-04-24 10:34:57 +1000

    Icon should be darker

--- src/components/ui/button-icon/button-icon.ts
+++ src/components/ui/button-icon/button-icon.ts
@@ -4,7 +4,7 @@ export const ButtonIcon = styled.a`
     color: ${props => props.theme.primaryColor};
     transition: filter 200ms ease-in-out, color 200ms ease-out;
     &:hover {
-        filter: drop-shadow(5px 3px 2px rgba(0, 0, 0, 0.7));
+        filter: drop-shadow(5px 3px 2px rgba(0, 0, 0, 1));
         color: ${props => props.theme.secondaryHighlight};
     }
 `;
